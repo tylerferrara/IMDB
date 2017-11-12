bcde2c50b7a26a0cdd533cbf4ff4af6d9fba29bc
Tyler Ferrara <tylerferrara123@gmail.com>
2017-11-12 14:16:38 -0500

    starter

---
+++ imdb/src/GraphSearchEngineImpl.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class GraphSearchEngineImpl implements GraphSearchEngine {
+
+	@Override
+	public List<Node> findShortestPath(Node s, Node t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ imdb/src/IMDBMoviesGraph.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Collection;
+
+public class IMDBMoviesGraph implements Graph {
+	
+	public IMDBMoviesGraph(String actorsFilename, String actressesFilename) throws IOException {
+		// TODO stuff
+	}
+
+	@Override
+	public Collection<? extends Node> getNodes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node getNodeByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
